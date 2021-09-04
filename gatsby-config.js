7dc10d08db1a79ddf8993724adb51fc3e2088c30
ktnkk <ktnkk@pm.me>
2021-09-04 11:15:19 +0900

    fix(gatsby-config): add a temporary trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,7 +130,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "",
+        trackingId: "test",
       },
     },
     `gatsby-plugin-sitemap`,
