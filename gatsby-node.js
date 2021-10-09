e68677cdea1ee02ef6356c10f8711e38eea0eed9
ktnkk <ktnkk@pm.me>
2021-10-09 11:44:55 +0900

    refactor(gatsby-node): modify to create articles asynchronously

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,87 +1,75 @@
 const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
 
-  return graphql(
-    `
-      {
-        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                date(formatString: "YYYY.MM.DD")
-                emoji
-                category
-                slug
-              }
+  const result = await graphql(`
+    {
+      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              date(formatString: "YYYY.MM.DD")
+              emoji
+              category
+              slug
             }
           }
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors;
     }
+  `);
 
-    const posts = result.data.allMarkdownRemark.edges;
+  if (result.errors) {
+    reporter.panicOnBuild(`There was an error loading your blog posts`, result.errors);
+    return;
+  }
 
-    // Create category posts pages
-    // ref: https://www.gatsbyjs.org/docs/adding-tags-and-categories-to-blog-posts/
-    let categories = [];
-    posts.forEach(post => {
-      if (post.node.frontmatter.category) {
-        categories.push(post.node.frontmatter.category);
+  const posts = result.data.allMarkdownRemark.edges;
+
+  let categories = [];
+  posts.forEach(post => {
+    if (post.node.frontmatter.category) {
+      categories.push(post.node.frontmatter.category);
+    }
+  });
+  categories = new Set(categories);
+  categories.forEach(category => {
+    createPage({
+      path: `/${category}/`,
+      component: path.resolve("src/templates/categories.js"),
+      context: {
+        category
       }
     });
-    categories = new Set(categories);
-    categories.forEach(category => {
-      createPage({
-        path: `/${category}/`,
-        component: path.resolve("src/templates/categories.js"),
-        context: {
-          category
-        }
-      });
+  });
+
+  let allRelatedPosts = {};
+  categories.forEach(category => {
+    let categoryPosts = posts.filter(post => {
+      return post.node.frontmatter.category === category;
+    });
+    allRelatedPosts[category] = categoryPosts ? categoryPosts.slice(0, 5) : [];
+  });
+
+  posts.forEach((post, index) => {
+    let relatedPosts = allRelatedPosts[post.node.frontmatter.category];
+    relatedPosts = relatedPosts.filter(relatedPost => {
+      return !(relatedPost.node.fields.slug === post.node.fields.slug);
     });
 
-    // get related Posts(retrive maximum 5 posts for each category)
-    let allRelatedPosts = {};
-    categories.forEach(category => {
-      let categoryPosts = posts.filter(post => {
-        return post.node.frontmatter.category === category;
-      });
-      allRelatedPosts[category] = categoryPosts ? categoryPosts.slice(0, 5) : [];
-    });
-
-    // Create blog posts pages.
-    posts.forEach((post, index) => {
-      // const previous =
-      //   index === posts.length - 1 ? null : posts[index + 1].node;
-      // const next = index === 0 ? null : posts[index - 1].node;
-
-      // setup related posts
-      // get the posts that has same categories.
-      let relatedPosts = allRelatedPosts[post.node.frontmatter.category];
-      // remove myself
-      relatedPosts = relatedPosts.filter(relatedPost => {
-        return !(relatedPost.node.fields.slug === post.node.fields.slug);
-      });
-
-      createPage({
-        path: post.node.frontmatter.slug,
-        component: path.resolve(`./src/templates/post.js`),
-        context: {
-          slug: post.node.fields.slug,
-          relatedPosts
-        }
-      });
+    createPage({
+      path: post.node.frontmatter.slug,
+      component: path.resolve(`./src/templates/post.js`),
+      context: {
+        slug: post.node.fields.slug,
+        relatedPosts
+      }
     });
   });
 };
