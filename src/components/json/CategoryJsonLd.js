6d8493eb42a5d0a952ec59b04b091c29910eea4e
ktnkk <ktnkk@pm.me>
2021-10-09 19:41:53 +0900

    refactor(json): modify to use useStaticQuery
    issue #7

--- src/components/json/CategoryJsonLd.js
+++ src/components/json/CategoryJsonLd.js
@@ -1,54 +1,49 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 const CategoryJsonLD = ({ categorySlug, categoryName }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query JsonLdCategoryQuery {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+      }
+    `
+  );
+
+  const { siteUrl } = site.siteMetadata;
+  const jsonBreadCrumbs = {
+    "@context": "http://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        item: {
+          "@id": siteUrl,
+          name: "HOME"
+        }
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        item: {
+          "@id": `${siteUrl}/${categorySlug}`,
+          name: categoryName
+        }
+      }
+    ]
+  };
+
   return (
-    <StaticQuery
-      query={jsonLdCategoryQuery}
-      render={data => {
-        const { siteUrl } = data.site.siteMetadata;
-        //bread crumbs
-        const jsonBreadCrumbs = {
-          "@context": "http://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              item: {
-                "@id": siteUrl,
-                name: "HOME"
-              }
-            },
-            {
-              "@type": "ListItem",
-              position: 2,
-              item: {
-                "@id": `${siteUrl}/${categorySlug}`,
-                name: categoryName
-              }
-            }
-          ]
-        };
-        return (
-          <Helmet>
-            <script type="application/ld+json">{JSON.stringify(jsonBreadCrumbs)}</script>
-          </Helmet>
-        );
-      }}
-    />
+    <Helmet>
+      <script type="application/ld+json">{JSON.stringify(jsonBreadCrumbs)}</script>
+    </Helmet>
   );
 };
 
 export default CategoryJsonLD;
-
-const jsonLdCategoryQuery = graphql`
-  query JsonLdCategoryQuery {
-    site {
-      siteMetadata {
-        siteUrl
-      }
-    }
-  }
-`;
