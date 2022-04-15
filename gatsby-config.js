8d57fd6b63d66d5ea4d0b73aa15e7bffc7c00064
ktnkk <ktnkk@pm.me>
2022-04-15 13:36:52 +0900

    perf(tsconfig): add new rules

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    siteMetadata: {
-        title: 'ktnkk.me',
-    },
-    plugins: [],
-};
-exports.default = config;
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-const Home = () => (react_1.default.createElement("div", null,
-    react_1.default.createElement("h1", null, "Hello world!")));
-exports.default = Home;
