526e0880a5f4bddd20fa2537954c4ee0016a9078
ktnkk <ktnkk@pm.me>
2022-04-17 15:45:46 +0900

    perf(storybook): introduce (but it doesn't work)

--- src/components/atoms/Paragraph/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Paragraph from '_/atoms/Paragraph';
-
-export default {
-  title: 'Paragraph',
-  component: Paragraph,
-};
-
-export const Default = () => <Paragraph />;
