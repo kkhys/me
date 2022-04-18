f46974396570c57488cedb5a90f8588ade72be42
ktnkk <ktnkk@pm.me>
2022-04-18 10:35:05 +0900

    perf(layout): add global css

--- src/layouts/Global.tsx
+++ src/layouts/Global.tsx
@@ -1,9 +1,19 @@
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import { EmotionContext } from '@/contexts';
 import type { FC, ReactNode } from 'react';
 
+const global = () => css`
+  html {
+    font-size: 62.5%;
+  }
+`;
+
 const GlobalLayout: FC<{ children: ReactNode }> = ({ children }) => (
-  <EmotionContext>{children}</EmotionContext>
+  <EmotionContext>
+    <Global styles={global()} />
+    {children}
+  </EmotionContext>
 );
 
 export default GlobalLayout;
