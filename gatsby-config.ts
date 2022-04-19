144354bd87205e9716ec0871c774b73c6c0610cc
ktnkk <ktnkk@pm.me>
2022-04-19 09:49:56 +0900

    perf(gatsby): setup transformer-remark and source-filesystem

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,29 @@
+import path from 'path';
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: 'ktnkk.me',
   },
-  plugins: ['gatsby-plugin-emotion'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: path.resolve('contents'),
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-autolink-headers',
+          'gatsby-remark-external-links',
+        ],
+      },
+    },
+  ],
 };
 
 export default config;
