6b48518a7e42ae9c2c60b4afcf98f2009d210d35
ktnkk <ktnkk@pm.me>
2022-04-23 17:09:02 +0900

    chore(templates): add test files

---
+++ src/templates/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const IndexTemplate: FC = () => (
+  <div>
+    <p>index template</p>
+  </div>
+);
+
+export default IndexTemplate;
---
+++ src/templates/post.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const PostTemplate: FC = () => (
+  <div>
+    <p>post template</p>
+  </div>
+);
+
+export default PostTemplate;
