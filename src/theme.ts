655a251496b8e52893efd8b5b87774f68bb587b7
ktnkk <ktnkk@pm.me>
2022-05-15 23:02:14 +0900

    perf(theme): add some objects

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,39 @@
 export const color = {
-  base: '#121212',
+  // primary: '',
+  // primaryDarker: '',
+  body: '#c9d1d9',
+  baseBackground: '#0d1117',
+};
+
+export const breakPoint = {
+  sp: 567,
+  tablet: 768,
+};
+
+export const mediaQuery = {
+  min: `@media screen and (min-width: ${breakPoint.sp}px), print`,
+  middleMin: `@media screen and (min-width: ${breakPoint.tablet}px), print`,
+  middleMax: `@media screen and (max-width: ${breakPoint.tablet - 0.02}px)`,
+  middle: `@media screen and (min-width: ${breakPoint.sp}px) and (max-width: ${
+    breakPoint.tablet - 0.02
+  }px)`,
+  max: `@media screen and (max-width: ${breakPoint.sp - 0.02}px)`,
+};
+
+export const layout = {
+  maxWidth: {
+    tablet: 760,
+    pc: 1100,
+  },
+  padding: {
+    sp: '0 2rem',
+    pc: '0 1.5rem',
+  },
 };
 
 export default {
   color,
+  breakPoint,
+  mediaQuery,
+  layout,
 };
