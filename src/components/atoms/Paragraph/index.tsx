51445e226e4625a8d197f6f2613e773be7a12917
ktnkk <ktnkk@pm.me>
2022-05-21 22:51:37 +0900

    chore(paragraph): make class name configurable

--- src/components/atoms/Paragraph/index.tsx
+++ src/components/atoms/Paragraph/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import * as styles from './styles';
+import type { Interpolation, Theme } from '@emotion/react';
 import type { ReactNode, FC } from 'react';
 
 type ParagraphRoleProps = 'normal' | 'link';
 
 type ParagraphProps = {
+  children: ReactNode;
+  className?: Interpolation<Theme>;
   tag?: 'p';
   size?: 's' | 'm' | 'l';
-  children: ReactNode;
 };
 
 const paragraphFactory: (role: ParagraphRoleProps) => FC<ParagraphProps> =
   (role) =>
-  ({ children, tag: Tag = 'p', size = 'm' }) =>
-    <Tag css={[styles[role], styles[size]]}>{children}</Tag>;
+  ({ children, className, tag: Tag = 'p', size = 'm' }) =>
+    <Tag css={[styles[role], styles[size], className]}>{children}</Tag>;
 
 const Paragraph = paragraphFactory('normal');
 export default Paragraph;
