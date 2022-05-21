d67a6e5be0a92cd1af245aa6db297d93a6e1a079
ktnkk <ktnkk@pm.me>
2022-05-21 19:20:51 +0900

    perf(atoms): add paragraph component

--- src/components/atoms/Paragraph/index.tsx
+++ src/components/atoms/Paragraph/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import type { FC } from 'react';
+import * as styles from './styles';
+import type { ReactNode, FC } from 'react';
 
-const Paragraph: FC = () => (
-  <>
-    <p>test</p>
-    <p>etst</p>
-  </>
-);
+type ParagraphRoleProps = 'normal' | 'link';
 
+type ParagraphProps = {
+  tag?: 'p';
+  size?: 's' | 'm' | 'l';
+  children: ReactNode;
+};
+
+const paragraphFactory: (role: ParagraphRoleProps) => FC<ParagraphProps> =
+  (role) =>
+  ({ children, tag: Tag = 'p', size = 'm' }) =>
+    <Tag css={[styles[role], styles[size]]}>{children}</Tag>;
+
+const Paragraph = paragraphFactory('normal');
 export default Paragraph;
+
+export const LinkParagraph = paragraphFactory('link');
