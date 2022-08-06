6abc19c8b91806380c6e4fd5ac6f7d47236546cd
Keiten Kiki <ktnkk@pm.me>
2022-08-06 21:48:09 +0900

    feat(heading): add new component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text } from '^/elements';
+import { Heading, Text } from '^/elements';
 
 const Home = () => (
   <>
     <h1 className='text-3xl font-bold underline'>Hello world!!!</h1>
     <Text>text test</Text>
+    <Heading>heading test</Heading>
   </>
 );
 
