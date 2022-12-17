24354089c5b69996ad6e1a44befc86312be7f129
ktnkk <ktnkk@pm.me>
2022-12-18 00:04:50 +0900

    style(*): exec prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.tsx",
-    "./src/components/**/*.tsx",
-  ],
+  content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
