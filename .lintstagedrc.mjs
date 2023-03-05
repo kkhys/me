3e73f289108905d491091d3f1dff5104b9f984d8
ktnkk <ktnkk@pm.me>
2023-03-05 23:44:45 +0900

    perf(lint staged): add prettier

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,9 +2,15 @@ import path from 'path';
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
+    .map((filename) => path.relative(process.cwd(), filename))
     .join(' --file ')}`;
+
+const buildPrettierCommand = (filenames) =>
+  `pnpm prettier --write ${filenames
+    .map((filename) => path.relative(process.cwd(), filename))
+    .join(' ')}`;
 
 export default {
   '*.{cjs,mjs,ts,tsx}': [buildEslintCommand],
+  '*.{cjs,mjs,ts,tsx,json,md,yml}': [buildPrettierCommand()]
 };
