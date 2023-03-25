9df1c5070cdf55a804128d8aa2d4373eae300355
ktnkk <ktnkk@pm.me>
2023-03-25 14:35:06 +0900

    feat(storybook): pnpm dlx storybook@next init

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: [
+    '../components/**/*.mdx',
+    '../components/**/*.stories.tsx',
+    '../features/**/*.mdx',
+    '../features/**/*.stories.tsx',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+
+export default config;
