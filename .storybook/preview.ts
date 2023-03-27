e31d8fe6fdbb5010e3ad8ff786abf1372a73f6f7
ktnkk <ktnkk@pm.me>
2023-03-27 22:57:13 +0900

    fix(package): add build storybook command

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,15 +2,7 @@ import type { Preview } from '@storybook/react';
 import '../styles/globals.css';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+  parameters: {},
 };
 
 export default preview;
