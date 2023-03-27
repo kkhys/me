61d0ebdc925f9f9cdf45d20e12ea662cb99f3d5f
ktnkk <ktnkk@pm.me>
2023-03-27 23:26:18 +0900

    chore(button): change color

--- components/general/Button/Button.tsx
+++ components/general/Button/Button.tsx
@@ -6,7 +6,7 @@ export const Button = () => {
   return (
     <button
       type='button'
-      className='rounded-md bg-indigo-500 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+      className='rounded-md bg-green-500 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
     >
       Button text
     </button>
