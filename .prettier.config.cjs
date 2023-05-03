fe48d29779272a72a17611a6e535a8e49afd1402
Keiten Kiki <ktnkk@pm.me>
2023-05-04 00:09:56 +0900

    refactor(prettier): 拡張子を json から cjs に変更

---
+++ .prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @typedef { import('prettier').Config } PrettierConfig */
+
+/** @type { PrettierConfig } */
+
+const config = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+}
+
+module.exports = config;
