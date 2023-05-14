31eeb2d9dc99bb76691c9a0c34ec5c4944f85957
Keiten Kiki <ktnkk@pm.me>
2023-05-14 14:08:30 +0900

    perf(prettier): import の並び順を修正するプラグインを追加

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,17 +1,40 @@
+/** @typedef { import('@ianvs/prettier-plugin-sort-imports').PluginConfig } SortImportsConfig */
 /** @typedef { import('prettier').Config } PrettierConfig */
 /** @typedef {{ tailwindConfig: string }} TailwindConfig */
 
-/** @type { PrettierConfig | TailwindConfig } */
+/** @type { SortImportsConfig | PrettierConfig | TailwindConfig } */
 
 const config = {
   jsxSingleQuote: true,
   singleQuote: true,
   trailingComma: 'all',
-  printWidth: 100,
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
   tailwindConfig: './packages/config/tailwind',
+  importOrder: [
+    '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
+    '^(next/(.*)$)|^(next$)',
+    '^(expo(.*)$)|^(expo$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@kkhys/(.*)$',
+    '',
+    '^#/utils/(.*)$',
+    '^#/features/(.*)$',
+    '^#/config/(.*)$',
+    '^#/ui/(.*)$',
+    '^#/styles/(.*)$',
+    '^#/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
 };
 
 module.exports = config;
