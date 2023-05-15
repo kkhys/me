59823b2fa402ece4386ea695330ff6a98a9dd474
ktnkk <ktnkk@pm.me>
2023-05-15 23:08:27 +0900

    refactor(web): ディレクトリ構造を変更

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -2,6 +2,7 @@ import type { MDXComponents } from 'mdx/types';
 
 export const useMDXComponents = (components: MDXComponents): MDXComponents => {
   return {
+    h1: ({ children }) => <h1 style={{ fontSize: '100px' }}>{children}</h1>,
     ...components,
   };
 };
