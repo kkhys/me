c60b2c43fbe407ccc4c7c979e49cd52f63f003e7
ktnkk <ktnkk@pm.me>
2023-05-17 00:44:01 +0900

    fix(web): eslint のエラー修正

--- apps/web/app/posts/[slug]/page.tsx
+++ apps/web/app/posts/[slug]/page.tsx
@@ -9,9 +9,7 @@ export const generateMetadata = ({
   params: { slug },
 }: {
   params: { slug: string };
-}) => {
-  return allPosts.find((post) => post.slug === slug).title;
-};
+}) => allPosts.find((post) => post.slug === slug).title;
 
 const PostLayout = ({ params: { slug } }: { params: { slug: string } }) => {
   const { title, publishedAt, body } = allPosts.find(
@@ -23,11 +21,8 @@ const PostLayout = ({ params: { slug } }: { params: { slug: string } }) => {
   return (
     <article className='mx-auto max-w-xl py-8'>
       <div className='mb-8 text-center'>
-        <time
-          dateTime={publishedAt as string}
-          className='mb-1 text-xs text-gray-600'
-        >
-          {format(parseISO(publishedAt as string), 'LLLL d, yyyy')}
+        <time dateTime={publishedAt} className='mb-1 text-xs text-gray-600'>
+          {format(parseISO(publishedAt), 'LLLL d, yyyy')}
         </time>
         <h1>{title}</h1>
       </div>
