a9c1d40c2be48266a60a23b33f00a3e7d93dfe98
ktnkk <ktnkk@pm.me>
2023-07-02 16:46:36 +0900

    feat(web): header に framer-motion を導入する

--- apps/web/features/global/ui/Header/Header.tsx
+++ apps/web/features/global/ui/Header/Header.tsx
@@ -9,7 +9,7 @@ import {
   type MotionStyle,
 } from 'framer-motion';
 
-import { Container, InnerContainer, OuterContainer } from '#/ui';
+import { InnerContainer, OuterContainer } from '#/ui';
 
 const navigationItem = [
   {
@@ -29,16 +29,25 @@ export const Header = forwardRef<HTMLDivElement, JSX.IntrinsicElements['div']>(
     const bgOpacityDark = useTransform(scrollY, [0, 72], [0.2, 0.8]);
 
     return (
-      <header className='fixed inset-x-0 top-0'>
+      <header className={clsx(className, 'fixed inset-x-0 top-0 z-10')}>
         <OuterContainer>
-          <div className='h-14 border-b border-gray-100 dark:border-gray-700/40'>
+          <motion.div
+            ref={ref}
+            className='h-14 border-b border-gray-100 bg-white/[var(--bg-opacity-light)] backdrop-blur-sm dark:border-gray-700/40 dark:bg-gray-1000/[var(--bg-opacity-dark)] dark:backdrop-blur'
+            style={
+              {
+                '--bg-opacity-light': bgOpacityLight,
+                '--bg-opacity-dark': bgOpacityDark,
+              } as unknown as MotionStyle
+            }
+          >
             <InnerContainer>
               <div className='flex items-center justify-between'>
                 <p>test</p>
                 <p>test2</p>
               </div>
             </InnerContainer>
-          </div>
+          </motion.div>
         </OuterContainer>
       </header>
     );
