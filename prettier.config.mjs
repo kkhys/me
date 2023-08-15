2248318ee57800ecd8e863c0566cf89f2e5821f7
ktnkk <ktnkk@pm.me>
2023-08-15 16:11:07 +0900

    chore(prettier): pnpm add -D prettier & add config (#1)

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @typedef  { import('prettier').Config } PrettierConfig */
+
+/** @type { PrettierConfig } */
+
+const config = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+export default config;
