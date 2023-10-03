85ca0290ed6a868b89ec1d27f1498490b22cb2ce
ktnkk <ktnkk@pm.me>
2023-10-03 22:46:58 +0900

    feat(ui): add button component (#5)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '#/styles/tailwind.css';
+import '#/styles/globals.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '#/components/ui/button';
+
 export default function HomePage() {
-  return <p className='text-sky-500'>Hello, world!</p>;
+  return (
+    <>
+      <p className='text-sky-500'>Hello, world!</p>
+      <Button>test</Button>
+    </>
+  );
 }
