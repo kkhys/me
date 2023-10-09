840f5385f84a3d05f401c923c1c043d34f2bcdad
ktnkk <ktnkk@pm.me>
2023-10-09 23:38:22 +0900

    feat(contentlayer): add a method to auto-generate post slugs (#6)

--- apps/web/src/lib/contentlayer/utils.ts
+++ apps/web/src/lib/contentlayer/utils.ts
@@ -1,3 +1,17 @@
-export function generateSlug(id: string) {
-  return id;
+import crypto from 'crypto';
+import bs58 from 'bs58';
+
+export function generateSlug(data: crypto.BinaryLike) {
+  const hashAlgorithm = 'sha512';
+  const encoding = 'hex' satisfies crypto.BinaryToTextEncoding;
+  const slugLength = 9;
+
+  const hashValue = crypto.createHash(hashAlgorithm).update(data).digest(encoding);
+  const hashArray = new Uint8Array(hashValue.length);
+
+  for (let i = 0; i < hashValue.length; i++) {
+    hashArray[i] = hashValue.charCodeAt(i);
+  }
+
+  return bs58.encode(hashArray).slice(0, slugLength);
 }
