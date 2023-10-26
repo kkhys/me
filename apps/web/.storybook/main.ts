2813e3c7d189896fea5949f151d907ef1d40ca8d
ktnkk <ktnkk@pm.me>
2023-10-27 00:41:33 +0900

    refactor(storybook): pnpm remove @storybook/addon-onboarding

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,16 +1,13 @@
 import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/nextjs';
 
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
+const getAbsolutePath = (value: string): any => dirname(require.resolve(join(value, 'package.json')));
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
