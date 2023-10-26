254e55e58acb35aef2eb7eaa0ec86d44e234fcaf
ktnkk <ktnkk@pm.me>
2023-10-27 00:29:50 +0900

    perf(e2e): add accessibility testing to playwright (#7)

--- apps/web/e2e/top.spec.ts
+++ apps/web/e2e/top.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@playwright/test';
+import { checkA11y, injectAxe } from 'axe-playwright';
 
 test.describe('Top page', () => {
   const path = '/';
@@ -6,5 +7,11 @@ test.describe('Top page', () => {
   test('has title', async ({ page }) => {
     await page.goto(path);
     await expect(page).toHaveTitle(/kkhys.me/);
+  });
+
+  test('accessibility verification', async ({ page }) => {
+    await page.goto(path);
+    await injectAxe(page);
+    await checkA11y(page);
   });
 });
