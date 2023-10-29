a5a040ca78ecb7e41168679b5a6c17db06658feb
ktnkk <ktnkk@pm.me>
2023-10-29 22:28:30 +0900

    chore(app): change as a result of component modifications (#74)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import '#/styles/globals.css';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='ja'>
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default Layout;
