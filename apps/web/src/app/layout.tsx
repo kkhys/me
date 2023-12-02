408afaacdedcf9e5da2af9827d88920b030cddac
ktnkk <ktnkk@pm.me>
2023-12-02 23:45:21 +0900

    feat(provider): add provider for the theme (#74)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import '#/styles/globals.css';
 
+import { ThemeProvider } from '#/lib/nextjs/theme-provider';
+import { Toaster } from '#/ui/feedback';
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='ja'>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 };
