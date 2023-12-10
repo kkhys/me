e38f2f910b8e33c392bba0212d15bddc4dbce67c
kkhys <kkhys@pm.me>
2023-12-10 16:01:26 +0900

    feat(ui): add global layout in UI feature
    A new component for global layout is added in the UI feature. It includes the layout definition in index.tsx and the layout story for storybook testing. The global layout comes with a 'background-lighter' class for a lighter background color.

---
+++ apps/web/src/ui/feature/global/layout/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export const Layout = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <div className='fixed inset-0 flex justify-center sm:px-8'>
+      <div className='flex w-full max-w-7xl lg:px-8'>
+        <div className='bg-background-lighter w-full ring-1 ring-zinc-100 dark:ring-zinc-300/20' />
+      </div>
+    </div>
+    <div className='relative flex w-full flex-col'>
+      {/*<Header />*/}
+      <main className='flex-auto'>{children}</main>
+      {/*<Footer />*/}
+    </div>
+  </>
+);
