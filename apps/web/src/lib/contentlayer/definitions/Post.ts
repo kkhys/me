d6daee62af1093be1c76f0dcc0f7f68d27110f59
kkhys <kkhys@pm.me>
2023-12-13 23:14:00 +0900

    fix(contentlayer): enforce strict typing in Post.ts
    Code in Post.ts has been revised to enforce strict typing when resolving attributes such as publishedAt, updatedAt, category, and tags. This would prevent possible type errors, maintaining code robustness. Additionally, new type imports (AllTagsTitle, CategoryTitle) have been introduced to clean up the code and improve readability.

--- apps/web/src/lib/contentlayer/definitions/Post.ts
+++ apps/web/src/lib/contentlayer/definitions/Post.ts
@@ -8,6 +8,7 @@ import { format, parseISO } from 'date-fns';
  * @see: https://github.com/contentlayerdev/contentlayer/issues/238
  */
 import { serverEnv } from '../../../env/index.mjs';
+import type { AllTagsTitle, CategoryTitle } from '../constants';
 import { allTagTitles, categoryTitles } from '../constants';
 import { generateCategoryObject, generateSlug, generateTagObject } from '../utils';
 
@@ -79,12 +80,12 @@ export const Post = defineDocumentType(() => ({
     publishedAtFormatted: {
       description: 'Formatted publication date and time',
       type: 'string',
-      resolve: ({ publishedAt }) => format(parseISO(publishedAt), 'LLLL d, yyyy'),
+      resolve: ({ publishedAt }) => format(parseISO(publishedAt as string), 'LLLL d, yyyy'),
     },
     updatedAtFormatted: {
       description: 'Formatted modification date and time',
       type: 'string',
-      resolve: ({ updatedAt }) => (updatedAt ? format(parseISO(updatedAt), 'LLLL d, yyyy') : undefined),
+      resolve: ({ updatedAt }) => (updatedAt ? format(parseISO(updatedAt as string), 'LLLL d, yyyy') : undefined),
     },
     slug: {
       description: 'Generate post slug from id',
@@ -94,14 +95,14 @@ export const Post = defineDocumentType(() => ({
     categoryObject: {
       description: 'Create a category object from a category title',
       type: 'json',
-      resolve: ({ category }) => generateCategoryObject(category),
+      resolve: ({ category }) => generateCategoryObject(category as CategoryTitle),
     },
     tagObjectList: {
       description: 'Create a list of tab objects from tag titles',
       type: 'list',
       resolve: ({ tags, category }) => {
         if (!tags) return undefined;
-        return Array.from(tags).map((tag) => generateTagObject(tag, category));
+        return Array.from(tags as AllTagsTitle[]).map((tag) => generateTagObject(tag, category as CategoryTitle));
       },
     },
   },
