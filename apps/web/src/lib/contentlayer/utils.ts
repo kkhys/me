bf4323ff3bb3337186d79f3094a30fefdc399da5
kkhys <kkhys@pm.me>
2023-12-15 18:02:42 +0900

    chore(lib): add utility methods and custom error class to 'contentlayer/utils'
    This commit introduces several utility methods and a custom error class (NotFoundError) to 'contentlayer/utils.ts'. These new methods will assist in generating unique slugs, extracting titles, retrieving specific category or tag objects, and verifying tag existence. A similar functionality, combining class values, is also added to 'shadcn-ui/utils.ts'.

--- apps/web/src/lib/contentlayer/utils.ts
+++ apps/web/src/lib/contentlayer/utils.ts
@@ -4,6 +4,12 @@ import bs58 from 'bs58';
 import type { AllTagsTitle, Base, Category, CategoryTitle, FashionTags, LifeTags, Tag, TechTags } from './constants';
 import { allTags, categories, fashionTags, lifeTags, techTags } from './constants';
 
+/**
+ * A custom error class representing a not found error.
+ *
+ * @extends Error
+ * @class
+ */
 class NotFoundError extends Error {
   constructor(message: string) {
     super(message);
@@ -11,6 +17,12 @@ class NotFoundError extends Error {
   }
 }
 
+/**
+ * Generates a unique slug based on the given data.
+ *
+ * @param data - The data used to generate the slug.
+ * @returns The generated slug.
+ */
 export const generateSlug = (data: crypto.BinaryLike) => {
   const hashAlgorithm = 'sha512';
   const encoding = 'hex' satisfies crypto.BinaryToTextEncoding;
@@ -26,8 +38,22 @@ export const generateSlug = (data: crypto.BinaryLike) => {
   return bs58.encode(hashArray).slice(0, slugLength);
 };
 
+/**
+ * Extracts the 'title' property from the given object.
+ *
+ * @template T - The type of object containing the 'title' property.
+ * @param item - The object from which to extract the 'title'.
+ * @return The value of the 'title' property.
+ */
 export const extractTitle = <T extends Pick<Base, 'title'>>(item: T) => item.title;
 
+/**
+ * Retrieves the category object based on the given title.
+ *
+ * @param title - The title of the category.
+ * @throws {NotFoundError} If the category is not found.
+ * @returns The category object with the matching title.
+ */
 const getCategoryFromTitle = (title: CategoryTitle) => {
   const category = categories.find((category) => category.title === title);
   if (!category) throw new NotFoundError(`Category not found: ${title}`);
@@ -36,6 +62,13 @@ const getCategoryFromTitle = (title: CategoryTitle) => {
   return category;
 };
 
+/**
+ * Retrieves a tag object based on its title.
+ *
+ * @param title - The title of the tag to retrieve.
+ * @returns The tag object associated with the given title.
+ * @throws {NotFoundError} If the tag with the given title is not found, or if the slug or emoji is missing.
+ */
 const getTagFromTitle = (title: AllTagsTitle) => {
   const tag = allTags.find((tag) => tag.title === title);
 
@@ -46,12 +79,26 @@ const getTagFromTitle = (title: AllTagsTitle) => {
   return tag;
 };
 
+/**
+ * Checks if a specific tag exists in a given target tag array.
+ *
+ * @param targetTag - The target tag array to search in.
+ * @param tagTitle - The title of the tag to check existence for.
+ * @throws {NotFoundError} if the tag is not found in the target tag array.
+ * @returns True if the tag exists, otherwise false.
+ */
 const isExistTag = <T extends TechTags | LifeTags | FashionTags>(targetTag: T, tagTitle: AllTagsTitle) => {
   if (!targetTag.map(extractTitle).includes(tagTitle)) throw new NotFoundError(`Tag not found: ${tagTitle}`);
 
   return true;
 };
 
+/**
+ * Generates a category object based on the given title.
+ *
+ * @param title - The title of the category.
+ * @returns The generated category object.
+ */
 export const generateCategoryObject = (title: CategoryTitle) => {
   const { slug, emoji } = getCategoryFromTitle(title);
 
@@ -62,6 +109,13 @@ export const generateCategoryObject = (title: CategoryTitle) => {
   } satisfies Category;
 };
 
+/**
+ * Generates a tag object based on the provided tag title and category.
+ *
+ * @param tagTitle - The title of the tag.
+ * @param category - The category of the tag.
+ * @returns The generated tag object.
+ */
 export const generateTagObject = (tagTitle: AllTagsTitle, category: CategoryTitle) => {
   switch (category) {
     case 'Tech':
