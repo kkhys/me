14787228b41e2f9c14aca2b6de10b8718473067e
kkhys <kkhys@pm.me>
2023-12-16 20:34:12 +0900

    feat(ui): add tooltips and external links to footer
    This update introduces a navigation link component with a tooltip feature to the website footer. It now includes external links accompanied by corresponding icons. The new 'NavLink' component is outfitted with features to handle tooltip content, child elements, and external link handling.

--- apps/web/src/ui/feature/global/footer/index.tsx
+++ apps/web/src/ui/feature/global/footer/index.tsx
@@ -1,4 +1,45 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { ArrowTopRightIcon } from '@radix-ui/react-icons';
+import { Book, FolderKanban } from 'lucide-react';
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '#/ui/data-display';
 import { ContainerInner, ContainerOuter } from '#/ui/feature/global';
+import { Button } from '#/ui/general';
+
+const NavLink = ({
+  href,
+  tooltipContent,
+  children,
+  isExternal = false,
+}: {
+  href: string;
+  tooltipContent: string;
+  children: React.ReactNode;
+  isExternal?: boolean;
+}) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button asChild variant='ghost' size='icon'>
+          <Link
+            href={href}
+            target={isExternal ? '_blank' : undefined}
+            rel={isExternal ? 'noopener noreferrer' : undefined}
+          >
+            {children}
+          </Link>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <div className='flex items-center'>
+          <p>{tooltipContent}</p>
+          {isExternal && <ArrowTopRightIcon className='ml-1 h-3 w-3' />}
+        </div>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
 
 export const Footer = () => (
   <footer className='mt-32 flex-none'>
@@ -6,10 +47,16 @@ export const Footer = () => (
       <div className='border-t border-zinc-100 pb-6 pt-6 dark:border-zinc-700/40'>
         <ContainerInner>
           <div className='flex flex-col items-center justify-between gap-6 sm:flex-row'>
-            {/*<div className='flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200'>*/}
-            {/*<NavLink href="/about">About</NavLink>*/}
-            {/*</div>*/}
-            <p className='text-sm text-zinc-400 dark:text-zinc-500'>
+            <div className='flex flex-wrap justify-center gap-x-1 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200'>
+              <NavLink href='https://github.com/users/kkhys/projects/3' tooltipContent='Roadmap' isExternal>
+                <FolderKanban className='h-4 w-4' />
+              </NavLink>
+              {/* TODO: add storybook link */}
+              <NavLink href='/' tooltipContent='Storybook' isExternal>
+                <Book className='h-4 w-4' />
+              </NavLink>
+            </div>
+            <p className='text-xs text-zinc-400 dark:text-zinc-500'>
               CC BY-NC-SA 4.0 2023-PRESENT &copy; Keisuke Hayashi
             </p>
           </div>
