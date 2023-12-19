c44f82b98b5ed03f0674d85b4e289980e5a61e72
kkhys <kkhys@pm.me>
2023-12-19 21:37:56 +0900

    feat(ui): add notoEmoji font and suppress hydration warning
    The notoEmoji font has been included in the layout.tsx file, enhancing the font options. An additional 'suppressHydrationWarning' property is added to the 'html' element to eliminate warnings regarding server and client rendering inconsistency.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import '#/styles/globals.css';
 
-import { inter, jetBrainsMono, newsreader, notoSansJP } from '#/lib/nextjs/fonts';
+import { inter, jetBrainsMono, newsreader, notoEmoji, notoSansJP } from '#/lib/nextjs/fonts';
 import { Providers } from '#/providers';
 import { Layout } from '#/ui/feature/global';
 import { Toaster } from '#/ui/feedback';
@@ -10,7 +10,8 @@ import { Toaster } from '#/ui/feedback';
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html
     lang='ja'
-    className={`h-full antialiased ${notoSansJP.className} ${inter.variable} ${newsreader.variable} ${jetBrainsMono.variable}`}
+    className={`h-full antialiased ${notoSansJP.className} ${inter.variable} ${newsreader.variable} ${jetBrainsMono.variable} ${notoEmoji.variable}`}
+    suppressHydrationWarning
   >
     <body className='flex h-full'>
       <Providers>
