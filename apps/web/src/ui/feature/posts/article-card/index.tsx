1e327143f5bdba533118a2e7fd6362dc5c2f22e4
kkhys <kkhys@pm.me>
2023-12-21 21:17:34 +0900

    feat(ui): add ArticleCard component to posts feature
    The ArticleCard component is created under posts feature. It displays a blog post in a card format, making use of the EyeCatch component to display emoji. The Link functionality is animated with framer-motion package.

---
+++ apps/web/src/ui/feature/posts/article-card/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+import type { Post } from 'contentlayer/generated';
+import { motion } from 'framer-motion';
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '#/ui/data-display';
+import { EyeCatch } from '#/ui/feature/posts';
+
+export const ArticleCard = ({ post }: { post: Post }) => {
+  const { slug, title, emoji, publishedAtFormatted } = post;
+  const MotionLink = motion(Link);
+
+  return (
+    <MotionLink href={`/posts/${slug}`} whileHover={{ scale: 1.03 }} whileTap={{ scale: 1.01 }} className='h-full'>
+      <Card className='flex h-full flex-col justify-between'>
+        <CardHeader>
+          <EyeCatch emoji={emoji} />
+        </CardHeader>
+        <CardContent>
+          <CardTitle>{title}</CardTitle>
+        </CardContent>
+        <CardFooter>
+          <CardDescription className='font-serif'>{publishedAtFormatted}</CardDescription>
+        </CardFooter>
+      </Card>
+    </MotionLink>
+  );
+};
