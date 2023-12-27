ed1e023dd8ab0bdbe738ab7db87f847b44ba6b01
kkhys <kkhys@pm.me>
2023-12-28 00:47:57 +0900

    feat(page): update UI and content on personal page
    The personal page UI has been updated with added import statements and altered structure, and the content has been refined. The updates emphasize information prioritization, visual simplicity, and convenient access to the blog posts with a redirected link button.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,7 @@
+import * as React from 'react';
 import type { Metadata } from 'next';
+import Link from 'next/link';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
 
 import { Container, FadeIn, FadeInStagger } from '#/ui/feature/global';
 import { Prose } from '#/ui/general';
@@ -10,22 +13,31 @@ export const metadata = {
 const Page = () => {
   return (
     <Container>
-      <Prose className='font-serif'>
-        <FadeInStagger>
-          <FadeIn>
-            <h1>Keisuke Hayashi</h1>
-          </FadeIn>
+      <FadeInStagger>
+        <FadeIn>
+          <h1 className='font-serif text-lg font-medium'>Keisuke Hayashi</h1>
+        </FadeIn>
+        <Prose className='font-serif'>
           <FadeIn>
             <p>
-              <span className='font-sans'>Passion for hacking</span>. Full-stack developer based in Tokyo with making
-              the most out of the least amount of effort. Love to make something and make people happy and surprised.
+              <span className='font-sans'>Passion for hacking</span>. Full-stack developer based in Tokyo. Love to make
+              something and make people happy and surprised.
             </p>
           </FadeIn>
           <FadeIn>
-            <p>In the past I’ve designed clothes and made patterns.</p>
+            <p>
+              I keep a blog to document my interests and my life. If you are interested, please take a look. Currently,
+              only Japanese articles are available.
+            </p>
           </FadeIn>
-        </FadeInStagger>
-      </Prose>
+          <FadeIn>
+            <Link href={{ pathname: '/posts' }} className='inline-flex items-center'>
+              Read blog
+              <ArrowRightIcon className='ml-1 h-3.5 w-3.5' />
+            </Link>
+          </FadeIn>
+        </Prose>
+      </FadeInStagger>
     </Container>
   );
 };
