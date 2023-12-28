3ad3454315a5a4b8d135cd58a0a6d976ae171f82
kkhys <kkhys@pm.me>
2023-12-28 10:12:46 +0900

    feat(page): add 404 'Page Not Found' UI and handling
    A new NotFound module has been created to handle 404 errors. This addition includes a clear error message and a button that redirects users back to the home page to enhance the user experience during website navigation.

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+import { Container, FadeIn, FadeInStagger } from '#/ui/feature/global';
+import { Button, Prose } from '#/ui/general';
+
+const NotFound = () => (
+  <Container className='font-serif'>
+    <FadeInStagger>
+      <FadeIn>
+        <h1 className='text-lg font-medium'>404 - Page Not Found</h1>
+      </FadeIn>
+      <FadeIn>
+        <Prose>
+          <p>Sorry, but the page you were trying to view does not exist.</p>
+        </Prose>
+      </FadeIn>
+      <FadeIn>
+        <Button variant='outline' asChild>
+          <Link href='/'>Go back home</Link>
+        </Button>
+      </FadeIn>
+    </FadeInStagger>
+  </Container>
+);
+
+export default NotFound;
