37dd581239c672795e4c68097672785f9e87c839
kkhys <kkhys@pm.me>
2024-01-02 11:18:12 +0900

    chore(seo): add metadata to maintenance page
    Integrated Metadata type from 'next' and utilized it to declare metadata details for the maintenance page. This includes setting the meta 'robots' property to 'noindex' which instructs search engines not to index the page, and providing a user-friendly title and description.

--- apps/web/src/app/maintenance/page.tsx
+++ apps/web/src/app/maintenance/page.tsx
@@ -1,9 +1,16 @@
+import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import { get } from '@vercel/edge-config';
 
 import { env } from '#/env.mjs';
 import { Container, FadeIn, FadeInStagger } from '#/ui/feature/global';
 import { Prose } from '#/ui/general';
+
+export const metadata = {
+  robots: 'noindex',
+  title: 'Maintenance',
+  description: 'It will be back up in a while, so please have a cup of coffee and relax.',
+} satisfies Metadata;
 
 const Page = async () => {
   if (!env.CI) {
