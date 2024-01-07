b1e75f59eac35a094cd98ab63aff0dad44dda896
kkhys <kkhys@pm.me>
2024-01-07 19:19:09 +0900

    chore(rehype): change rehypePrettyCodeOptions to satisfy Options
    Modifying the rehypePrettyCodeOptions constant to satisfy Options rather than Partial<Options>. As a result, ensuring that the correct properties are included, allowing for better code presentation.

--- apps/web/src/lib/mdx/rehype.ts
+++ apps/web/src/lib/mdx/rehype.ts
@@ -3,4 +3,4 @@ import type { Options } from 'rehype-pretty-code';
 export const rehypePrettyCodeOptions = {
   theme: 'poimandres',
   keepBackground: false,
-} satisfies Partial<Options>;
+} satisfies Options;
