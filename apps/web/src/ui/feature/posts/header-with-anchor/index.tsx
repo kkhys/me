16fe6a1bb21e385e05e1f77f8962778947974b5e
kkhys <kkhys@pm.me>
2024-01-20 12:22:52 +0900

    fix(ui): improve styling for post's Anchor component
    The Anchor component in the posts header has been restyled. Significant changes were made to hover effects and visibility behavior, enhancing visual experience and component interaction.

--- apps/web/src/ui/feature/posts/header-with-anchor/index.tsx
+++ apps/web/src/ui/feature/posts/header-with-anchor/index.tsx
@@ -18,9 +18,9 @@ const AnchorIcon = (props: React.ComponentPropsWithoutRef<'svg'>) => (
 );
 
 const Anchor = ({ id, inView, children }: { id: string; inView: boolean; children: React.ReactNode }) => (
-  <Link href={`#${id}`} className='group text-inherit no-underline hover:text-inherit'>
+  <Link href={`#${id}`} className='group no-underline'>
     {inView && (
-      <div className='absolute ml-[calc(-1*var(--width))] mt-1 hidden w-[var(--width)] opacity-0 transition [--width:calc(2.625rem+0.5px+50%-min(50%,calc(theme(maxWidth.lg)+theme(spacing.8))))] group-hover:opacity-100 group-focus:opacity-100 md:block lg:z-50 2xl:[--width:theme(spacing.10)]'>
+      <div className='absolute ml-[calc(-1*var(--width))] mt-1 hidden w-[var(--width)] opacity-0 transition [--width:calc(2.625rem+0.5px+50%-min(50%,calc(theme(maxWidth.lg)+theme(spacing.8))))] group-hover:opacity-100 group-focus:opacity-100 md:inline-block 2xl:[--width:theme(spacing.10)]'>
         <div className='group/anchor bg-secondary ring-0.5 block size-5 rounded-lg ring-inset ring-zinc-300 transition hover:ring-zinc-500 dark:ring-zinc-700 dark:hover:bg-zinc-700 dark:hover:ring-zinc-600'>
           <AnchorIcon className='size-5 stroke-zinc-500 transition dark:stroke-zinc-400 dark:group-hover/anchor:stroke-white' />
         </div>
