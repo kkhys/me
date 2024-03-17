36b1712aeb94b464a1d1b59bafee18c71f6e04f1
kkhys <kkhys@pm.me>
2024-03-17 12:20:14 +0900

    chore(ui): refactor UI package configuration in npm
    Updated the tailwind.config.ts to pull content from baseConfig. Also, updated tsconfig.json file to extend from "@kkhys/tsconfig/vite.json". In addition, few compiler options like 'baseUrl', 'declaration', and 'declarationMap' were introduced.

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -7,6 +7,6 @@ import type { Config } from 'tailwindcss';
 import baseConfig from '@kkhys/tailwind-config';
 
 export default {
-  content: ['./src/**/*.tsx'],
+  content: [...baseConfig.content],
   presets: [baseConfig],
 } satisfies Config;
