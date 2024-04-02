4b1a29c9796447e8b86ed9cea2bc5da47c6a7ef5
kkhys <kkhys@pm.me>
2024-04-03 00:16:37 +0900

    build(db): update schema/migrations for PostgreSQL support in 'db' package
    This commit modifies the 'db' package to support PostgreSQL database. The migration files and the schema have been changed from MySQL to PostgreSQL settings. This commit enhances the application's compatibility with PostgreSQL and updates the corresponding migration methods.

--- packages/db/src/schema/migrations/0000_white_trauma.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `me_post` (
-	`id` varchar(255) NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`updatedAt` timestamp ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT `me_post_id` PRIMARY KEY(`id`),
-	CONSTRAINT `me_post_name_unique` UNIQUE(`name`)
-);
