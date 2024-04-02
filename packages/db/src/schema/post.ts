a4b13034ce9e11cd5b71a694994ed9c767c6f9ea
kkhys <kkhys@pm.me>
2024-04-03 00:16:22 +0900

    fix(db): update schema for PostgreSQL in 'db' package
    Changed 'db' package to use PostgreSQL, replacing MySQL. This included updating the drivers and table creation methods as well as connection strings. These changes contribute to enhanced application functionality in tandem with the new database system.

--- packages/db/src/schema/post.ts
+++ packages/db/src/schema/post.ts
@@ -1,13 +1,17 @@
-import { sql } from 'drizzle-orm';
-import { timestamp, varchar } from 'drizzle-orm/mysql-core';
+import { integer, timestamp, uniqueIndex, varchar } from 'drizzle-orm/pg-core';
 
-import { mySqlTable } from './_table';
+import { pgTable } from './_table';
 
-export const post = mySqlTable('post', {
-  id: varchar('id', { length: 255 }).primaryKey(),
-  slug: varchar('name', { length: 255 }).notNull().unique(),
-  createdAt: timestamp('created_at')
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
-  updatedAt: timestamp('updatedAt').onUpdateNow(),
-});
+export const posts = pgTable(
+  'post',
+  {
+    slug: varchar('slug', { length: 255 }).primaryKey(),
+    views: integer('views').default(0).notNull(),
+    likes: integer('likes').default(0).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt').defaultNow().notNull(),
+  },
+  ({ slug }) => ({
+    slugIdx: uniqueIndex('slug_idx').on(slug),
+  }),
+);
