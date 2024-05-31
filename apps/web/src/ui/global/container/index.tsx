63339cdf606fbb0ea6fa8b07b8e08a333b4b3e31
kkhys <kkhys@pm.me>
2024-05-31 23:56:07 +0900

    chore(ui): update ui element styles in container and back-button components
    Adjusted padding in the Container component and updated the alignment of the back-button. The Container component now has more padding and the back-button's positioning was changed to fixed from absolute, with adjusted translation for better alignment.

--- apps/web/src/ui/global/container/index.tsx
+++ apps/web/src/ui/global/container/index.tsx
@@ -15,8 +15,8 @@ export const ContainerOuter = React.forwardRef<React.ElementRef<'div'>, React.Co
 export const ContainerInner = React.forwardRef<React.ElementRef<'div'>, React.ComponentPropsWithoutRef<'div'>>(
   function InnerContainer({ className, children, ...props }, ref) {
     return (
-      <div ref={ref} className={cn('px-4 sm:px-8 lg:px-12 xl:relative', className)} {...props}>
-        <div className='mx-auto max-w-2xl'>{children}</div>
+      <div ref={ref} className={cn('px-6 sm:px-8 lg:px-12', className)} {...props}>
+        <div className='relative mx-auto max-w-2xl'>{children}</div>
       </div>
     );
   },
