6ecce3e0feba0760f6cd8889b62dec917dfa7f4e
kkhys <kkhys@pm.me>
2024-12-14 09:13:31 +0900

    refactor(ui): update homepage content with personal details and blog link
    Replaced placeholder text with structured content about the author, including a brief introduction and a link to the blog. The blog section encourages users to explore posts, improving the page's clarity and user engagement.

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,8 +1,30 @@
+import { Prose } from "@kkhys/ui";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
+
 const Page = () => {
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <>
+      <h1 className="font-sans font-medium">Keisuke Hayashi</h1>
+      <Prose className="font-sans">
+        <p>
+          Passion for hacking. Full-stack developer based in Tokyo. Love to make
+          something and make people happy and surprised.
+        </p>
+        <p>
+          I keep a blog to document my interests and my life. If you are
+          interested, please take a look. Currently, only Japanese articles are
+          available.
+        </p>
+        <Link
+          href={{ pathname: "/posts" }}
+          className="inline-flex items-center"
+        >
+          Read blog
+          <MoveRight className="ml-1 size-3.5" />
+        </Link>
+      </Prose>
+    </>
   );
 };
 
