f5ad9ec0521fdc0f94d84f32fd237d747aff18e2
kkhys <kkhys@pm.me>
2024-12-15 11:35:44 +0900

    refactor(web): add dynamic image generation for app icons and OpenGraph
    Introduces image generation for Apple icons, app favicon, OpenGraph, and Twitter cards using Next.js edge runtime. These components fetch custom fonts and style the images dynamically. Also includes a minor script update in `package.json`.

---
+++ apps/web/src/app/icon.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = "image/png";
+
+const Icon = async () => {
+  const interMedium = await fetch(
+    new URL("../../assets/fonts/Inter-Medium.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    <div
+      style={{
+        fontSize: 18,
+        background: "#0a0a0b",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Inter",
+        color: "#e4e4e7",
+        paddingTop: 3,
+      }}
+    >
+      K
+    </div>,
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: interMedium,
+          style: "normal",
+          weight: 500,
+        },
+      ],
+    },
+  );
+};
+
+export default Icon;
---
+++ apps/web/src/app/twitter-image.tsx
@@ -0,0 +1,50 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const alt = "About me";
+
+export const size = {
+  width: 500,
+  height: 500,
+};
+
+export const contentType = "image/png";
+
+const Image = async () => {
+  const interMedium = await fetch(
+    new URL("../../assets/fonts/Inter-Medium.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    <div
+      style={{
+        fontSize: 260,
+        background: "#0a0a0b",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Inter",
+        color: "#e4e4e7",
+        paddingTop: 25,
+      }}
+    >
+      K
+    </div>,
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: interMedium,
+          style: "normal",
+          weight: 500,
+        },
+      ],
+    },
+  );
+};
+
+export default Image;
