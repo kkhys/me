2902f017e480ff8a38e3a88a89c6aa0b3bdca33a
kkhys <kkhys@pm.me>
2024-12-19 23:58:20 +0900

    refactor(robots): add robots.txt generation to block specific crawlers
    Introduced a new robots.ts file to define rules for specific user agents, disallowing their access to the site. This also includes sitemap and host directives, leveraging the siteConfig for dynamic URL handling.

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,45 @@
+import type { MetadataRoute } from "next";
+import { siteConfig } from "#/config";
+
+const robots = (): MetadataRoute.Robots => {
+  return {
+    rules: [
+      {
+        userAgent: "Yahoo Pipes 1.0",
+        disallow: "/",
+      },
+      {
+        userAgent: "008",
+        disallow: "/",
+      },
+      {
+        userAgent: "voltron",
+        disallow: "/",
+      },
+      {
+        userAgent: "Bytespider",
+        disallow: "/",
+      },
+      {
+        userAgent: "Livelapbot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Megalodon",
+        disallow: "/",
+      },
+      {
+        userAgent: "ia_archiver",
+        disallow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+    ],
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+    host: siteConfig.url,
+  };
+};
+
+export default robots;
