4e758a14b68d47992b313411eb3d90f389ccdcdb
kkhys <kkhys@pm.me>
2025-01-05 13:04:13 +0900

    chore(ui): add Storybook stories for Google Maps and Pager components
    This commit introduces new Storybook stories for the Google Maps Block and Prev & Next Pager components. These stories provide examples and configurations to showcase their usage and improve UI development efficiency.

---
+++ apps/web/src/app/posts/_ui/blocks/google-maps-block/index.stories.tsx
@@ -0,0 +1,49 @@
+import { Prose } from "@kkhys/ui";
+import type { Meta, StoryObj } from "@storybook/react";
+import { GoogleMapsBlock } from "#/app/posts/_ui";
+const meta = {
+  title: "Post / Block / Google Maps Block",
+  component: GoogleMapsBlock,
+  argTypes: {
+    placeId: {
+      control: "text",
+      description: "The place ID of the location to show.",
+      table: {
+        type: { summary: "string" },
+      },
+      type: {
+        name: "string",
+      },
+    },
+    caption: {
+      control: "text",
+      description: "The caption of the map.",
+      table: {
+        type: { summary: "string" },
+      },
+      type: {
+        name: "string",
+      },
+    },
+  },
+  args: {
+    placeId: "ChIJ2-L1uEhbFkcR-Zek84Ap7zI",
+  },
+  decorators: [
+    (Story) => (
+      <div className="w-full max-w-2xl mx-auto">
+        <Prose>
+          <Story />
+        </Prose>
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof GoogleMapsBlock>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Default = {} satisfies Story;
+export const WithCaption = {
+  args: {
+    caption: "旧オスカー・シンドラーのホーロー工場",
+  },
+} satisfies Story;
