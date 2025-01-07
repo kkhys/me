77ca39d207e8d5c9f7784ba41c807f321499b761
kkhys <kkhys@pm.me>
2025-01-07 21:06:55 +0900

    chore(command): add script to generate shared content files
    Introduces a script to dynamically generate `post-emojis.ts` and `search-items.ts` for better content management. The `generate:files` command is added to the package.json to streamline the generation process during development.

---
+++ apps/web/src/share/post-emojis.ts
@@ -0,0 +1,130 @@
+export const postEmojis = [
+  {
+    slug: "p1vdu33",
+    emoji: "💺",
+  },
+  {
+    slug: "p1tnt9a",
+    emoji: "🔩",
+  },
+  {
+    slug: "p1wcla5",
+    emoji: "🏺",
+  },
+  {
+    slug: "p1e0lpm",
+    emoji: "🏃",
+  },
+  {
+    slug: "p18vcqd",
+    emoji: "🍫",
+  },
+  {
+    slug: "p1v00e8",
+    emoji: "🏝️",
+  },
+  {
+    slug: "p1n03k6",
+    emoji: "🌜",
+  },
+  {
+    slug: "p1t6el8",
+    emoji: "🇪🇪",
+  },
+  {
+    slug: "p15p4yn",
+    emoji: "⛵️",
+  },
+  {
+    slug: "p1672pu",
+    emoji: "📷",
+  },
+  {
+    slug: "p1v6tlh",
+    emoji: "🖥️",
+  },
+  {
+    slug: "p1qhr64",
+    emoji: "📗",
+  },
+  {
+    slug: "p1ppkfs",
+    emoji: "🖥️",
+  },
+  {
+    slug: "p1rklfz",
+    emoji: "🚀",
+  },
+  {
+    slug: "p1y4nft",
+    emoji: "🔗",
+  },
+  {
+    slug: "p1fw2ts",
+    emoji: "🔠",
+  },
+  {
+    slug: "p1eemm6",
+    emoji: "🧜‍♀️",
+  },
+  {
+    slug: "p1g6z2d",
+    emoji: "🚀",
+  },
+  {
+    slug: "p16ceda",
+    emoji: "🔨",
+  },
+  {
+    slug: "p1r60de",
+    emoji: "🚀",
+  },
+  {
+    slug: "p1v9jvx",
+    emoji: "🚀",
+  },
+  {
+    slug: "p1srf75",
+    emoji: "🔼",
+  },
+  {
+    slug: "p1ua4wh",
+    emoji: "🔌",
+  },
+  {
+    slug: "p15e6x7",
+    emoji: "🔄",
+  },
+  {
+    slug: "p1c8jpk",
+    emoji: "🚀",
+  },
+  {
+    slug: "p1gvayx",
+    emoji: "🧈",
+  },
+  {
+    slug: "p164vu8",
+    emoji: "🔑",
+  },
+  {
+    slug: "p128uug",
+    emoji: "🎭",
+  },
+  {
+    slug: "p1kqv7s",
+    emoji: "🚀",
+  },
+  {
+    slug: "p1ydlf6",
+    emoji: "🛢️",
+  },
+  {
+    slug: "p143t9d",
+    emoji: "🚀",
+  },
+  {
+    slug: "p1ffkqq",
+    emoji: "🦣",
+  },
+];
