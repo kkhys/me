9e7e893551a8006451924a0f5996884ed366a2df
kkhys <kkhys@pm.me>
2025-02-02 20:27:40 +0900

    fix(ui): add `className` prop to Container component
    This update allows users to pass custom class names to the Container component for greater styling flexibility. Also, a redundant `mx-auto` was moved to the outer div to clean up the structure.

--- apps/web/src/ui/container/index.tsx
+++ apps/web/src/ui/container/index.tsx
@@ -3,16 +3,19 @@ import { cn } from "@kkhys/ui";
 export const Container = ({
   width = "narrow",
   children,
+  className,
 }: {
   width?: "narrow" | "wide";
   children: React.ReactNode;
+  className?: string;
 }) => (
   <div
     className={cn(
       "w-full mx-auto",
       width === "narrow" ? "max-w-2xl" : "max-w-5xl",
+      className,
     )}
   >
-    <div className="container py-12">{children}</div>
+    <div className="container py-12 mx-auto">{children}</div>
   </div>
 );
