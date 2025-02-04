f506588e77f92dcc1fcb812d6b2324e0658ccff9
kkhys <kkhys@pm.me>
2025-02-04 22:54:15 +0900

    chore(photo): add JSON-LD for SEO and enhance metadata generation
    Introduced JSON-LD to provide structured data for better SEO, including a breadcrumb list for the photos page. Updated the `generateMetadata` function to include Open Graph and other metadata for improved sharing and discovery. These changes improve the site's semantic structure and visibility.

--- apps/web/src/app/photos/[slug]/page.tsx
+++ apps/web/src/app/photos/[slug]/page.tsx
@@ -6,13 +6,83 @@ import {
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import Zoom from "react-medium-image-zoom";
-import type { Camera, Lens } from "#/config";
+import { type Camera, type Lens, siteConfig } from "#/config";
 import { getPhotoBySlug, getPublicPhotos } from "#/utils/photo";
 import "#/styles/react-medium-image-zoom.css";
 import "../_styles/index.css";
+import type { Photo } from "contentlayer/generated";
+import * as React from "react";
+import type { BreadcrumbList, WithContext } from "schema-dts";
+
+const JsonLd = ({ photo: { title, slug } }: { photo: Photo }) => {
+  const jsonLdBreadcrumbList = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: siteConfig.name,
+        item: siteConfig.url,
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Photo",
+        item: `${siteConfig.url}/photos`,
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        name: title,
+        item: `${siteConfig.url}/photos/${slug}`,
+      },
+    ],
+  } satisfies WithContext<BreadcrumbList>;
+
+  return (
+    <script
+      type="application/ld+json"
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(jsonLdBreadcrumbList),
+      }}
+    />
+  );
+};
 
 export const generateStaticParams = async () =>
   getPublicPhotos().map(({ slug }) => ({ slug }));
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
+  const photo = getPhotoBySlug(slug);
+
+  if (!photo) {
+    return;
+  }
+
+  const { title, publishedAt, updatedAt } = photo;
+  const url = `/photos/${slug}`;
+
+  return {
+    title,
+    description: "The photo was taken by Keisuke Hayashi.",
+    alternates: {
+      canonical: `/photos/${slug}`,
+    },
+    openGraph: {
+      type: "article",
+      url,
+      publishedTime: publishedAt,
+      modifiedTime: updatedAt ?? undefined,
+    },
+  };
+};
 
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
@@ -38,41 +108,44 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const lensData = photo.lensData as Lens;
 
   return (
-    <div className="space-y-6">
-      <Zoom zoomImg={{ src: path }}>
-        <Image
-          src={path}
-          width={width}
-          height={height}
-          sizes="100vw"
-          alt={title}
-          placeholder="blur"
-          blurDataURL={blurDataURL}
-          quality={90}
-        />
-      </Zoom>
-      <h1 className="font-sans font-medium">{title}</h1>
-      <DescriptionList className="font-sans">
-        <DescriptionTerm>Camera</DescriptionTerm>
-        <DescriptionDetails>
-          {cameraData.name} ({cameraData.manufacturer})
-        </DescriptionDetails>
-        <DescriptionTerm>Lens</DescriptionTerm>
-        <DescriptionDetails>
-          {lensData.name} ({lensData.manufacturer})
-        </DescriptionDetails>
-        <DescriptionTerm>F-number</DescriptionTerm>
-        <DescriptionDetails>{fNumber}</DescriptionDetails>
-        <DescriptionTerm>Focal length</DescriptionTerm>
-        <DescriptionDetails>{focalLength}mm</DescriptionDetails>
-        <DescriptionTerm>Shutter speed</DescriptionTerm>
-        <DescriptionDetails>{shutterSpeed}s</DescriptionDetails>
-        <DescriptionTerm>ISO</DescriptionTerm>
-        <DescriptionDetails>{iso}</DescriptionDetails>
-        <DescriptionTerm>Shot on</DescriptionTerm>
-        <DescriptionDetails>{publishedAtFormattedIso}</DescriptionDetails>
-      </DescriptionList>
-    </div>
+    <>
+      <JsonLd photo={photo} />
+      <div className="space-y-6">
+        <Zoom zoomImg={{ src: path }}>
+          <Image
+            src={path}
+            width={width}
+            height={height}
+            sizes="100vw"
+            alt={title}
+            placeholder="blur"
+            blurDataURL={blurDataURL}
+            quality={90}
+          />
+        </Zoom>
+        <h1 className="font-sans font-medium">{title}</h1>
+        <DescriptionList className="font-sans">
+          <DescriptionTerm>Camera</DescriptionTerm>
+          <DescriptionDetails>
+            {cameraData.name} ({cameraData.manufacturer})
+          </DescriptionDetails>
+          <DescriptionTerm>Lens</DescriptionTerm>
+          <DescriptionDetails>
+            {lensData.name} ({lensData.manufacturer})
+          </DescriptionDetails>
+          <DescriptionTerm>F-number</DescriptionTerm>
+          <DescriptionDetails>{fNumber}</DescriptionDetails>
+          <DescriptionTerm>Focal length</DescriptionTerm>
+          <DescriptionDetails>{focalLength}mm</DescriptionDetails>
+          <DescriptionTerm>Shutter speed</DescriptionTerm>
+          <DescriptionDetails>{shutterSpeed}s</DescriptionDetails>
+          <DescriptionTerm>ISO</DescriptionTerm>
+          <DescriptionDetails>{iso}</DescriptionDetails>
+          <DescriptionTerm>Shot on</DescriptionTerm>
+          <DescriptionDetails>{publishedAtFormattedIso}</DescriptionDetails>
+        </DescriptionList>
+      </div>
+    </>
   );
 };
 
