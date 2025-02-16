9a27601df61631be80180a1fac79db5dcb248999
kkhys <kkhys@pm.me>
2025-02-16 11:55:12 +0900

    chore(schema): refactor user table schema and update migrations.
    Removed timezone requirement from `createdAt` and `updatedAt` columns in both the schema and migration files. Recreated migration files to reflect the updated schema while ensuring consistency across the database layer.

--- packages/db/migrations/0000_nosy_proudstar.sql
+++ packages/db/migrations/0000_nosy_proudstar.sql
@@ -2,7 +2,7 @@ CREATE TABLE "user" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"clerk_id" varchar(255) NOT NULL,
 	"deleted_at" timestamp,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp NOT NULL,
 	CONSTRAINT "user_clerkId_unique" UNIQUE("clerk_id")
 );
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -4,8 +4,8 @@ export const User = pgTable("user", (t) => ({
   id: t.uuid().notNull().primaryKey().defaultRandom(),
   clerkId: t.varchar({ length: 255 }).notNull().unique(),
   deletedAt: t.timestamp(),
-  createdAt: t.timestamp({ withTimezone: true }).defaultNow().notNull(),
-  updatedAt: t.timestamp({ withTimezone: true }).$onUpdateFn(() => new Date()),
+  createdAt: t.timestamp().defaultNow().notNull(),
+  updatedAt: t.timestamp().notNull().$onUpdateFn(() => new Date()),
 }));
 
 export type InsertUser = typeof User.$inferInsert;
