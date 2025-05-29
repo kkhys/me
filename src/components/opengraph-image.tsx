a0b25a47024df00bdc5b9247a3f86f57886bd37a
kkhys <kkhys@pm.me>
2025-05-29 13:55:02 +0900

    feat(ogp): add dynamic OpenGraph image generation for blog posts
    Implemented a new API route to dynamically generate OpenGraph images using blog post emojis. Updated SEO components and blog layouts to utilize the generated images. Added required assets and dependencies for image rendering.

---
+++ src/components/opengraph-image.tsx
@@ -0,0 +1,49 @@
+/** @jsxImportSource react */
+/** @jsxRuntime automatic */
+
+import satori from "satori";
+import sharp from "sharp";
+
+export const opengraphImage = async ({ emoji }: { emoji: string }) => {
+  const firstEmoji = Array.from(emoji)[0];
+
+  const notoEmojiSemiBold = await Bun.file(
+    "./src/assets/NotoEmoji-SemiBold.ttf",
+  ).arrayBuffer();
+
+  const svg = await satori(
+    <div
+      style={{
+        fontSize: 250,
+        background: "#0a0a0b",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Noto Emoji",
+        color: "#e4e4e7",
+      }}
+    >
+      {firstEmoji}
+    </div>,
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Noto Emoji",
+          data: notoEmojiSemiBold,
+          style: "normal",
+          weight: 600,
+        },
+      ],
+    },
+  );
+
+  return sharp(Buffer.from(svg))
+    .png({
+      quality: 60,
+    })
+    .toBuffer();
+};
---
+++ src/pages/api/og/[id].png.ts
@@ -0,0 +1,30 @@
+import type { APIRoute, GetStaticPaths } from "astro";
+import { opengraphImage } from "#/components/opengraph-image.tsx";
+import { getPublicBlogEntries } from "#/lib/blog";
+
+export const getStaticPaths = (async () =>
+  (await getPublicBlogEntries()).map((entry) => {
+    return {
+      params: {
+        id: entry.id,
+      },
+      props: {
+        entry,
+      },
+    };
+  })) satisfies GetStaticPaths;
+
+export const GET: APIRoute = async ({ props }) => {
+  const {
+    entry: {
+      data: { emoji },
+    },
+  } = props;
+
+  if (!emoji) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const image = await opengraphImage({ emoji });
+  return new Response(image);
+};
