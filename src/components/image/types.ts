7f6300c95bbefcbae61152f61110e54f1ec9b93b
kkhys <kkhys@pm.me>
2025-06-02 11:33:17 +0900

    chore(image): remove `markdown-image` component and simplify image handling
    Replaced `markdown-image` usage with a unified `image` component in `image-handler`. Cleaned up unnecessary types and streamlined the implementation for better maintenance.

--- src/components/image/types.ts
+++ src/components/image/types.ts
@@ -1,20 +1,5 @@
 import type { LocalImageProps, RemoteImageProps } from "astro:assets";
-import type { HTMLAttributes } from "astro/types";
-
-export const alignClass = {
-  top: {
-    wrapper: "bg-top",
-    image: "object-top",
-  },
-  center: {
-    wrapper: "bg-center",
-    image: "object-center",
-  },
-};
 
 export type CustomImageProps = {
-  wrapperProps?: HTMLAttributes<"div">;
   ambientMode?: boolean;
-  align?: keyof typeof alignClass;
-  canZoom?: boolean;
 } & (LocalImageProps | RemoteImageProps);
