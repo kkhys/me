a3c26c21f9974ef2e8991909ef48f5e502807090
kkhys <kkhys@pm.me>
2025-06-10 22:48:03 +0900

    refactor(blog): remove unused `NODE_ENV` references and update Redis initialization method
    Cleared out `NODE_ENV` usages in blog actions for cleaner code. Switched Redis initialization to use explicit environment variables (`UPSTASH_REDIS_REST_URL` and `UPSTASH_REDIS_REST_TOKEN`) for better configuration clarity.

--- src/features/blog/actions/page-view.ts
+++ src/features/blog/actions/page-view.ts
@@ -1,5 +1,5 @@
 import { defineAction } from "astro:actions";
-import { NODE_ENV } from "astro:env/client";
+// import { NODE_ENV } from "astro:env/client";
 import { z } from "astro:schema";
 import { redis } from "#/lib/redis";
 
@@ -8,8 +8,8 @@ export const pageView = {
     input: z.object({ slug: z.string() }),
     handler: async (input) => {
       const { slug } = input;
-      const env = NODE_ENV;
-      console.log("env", env);
+      // const env = NODE_ENV;
+      // console.log("env", env);
       return (
         (await redis.get<number>(
           ["pageviews", "production", slug].join(":"),
@@ -21,8 +21,8 @@ export const pageView = {
     input: z.object({ slug: z.string() }),
     handler: async (input) => {
       const { slug } = input;
-      const env = NODE_ENV;
-      console.log("env", env);
+      // const env = NODE_ENV;
+      // console.log("env", env);
       await redis.incr(["pageviews", "production", slug].join(":"));
     },
   }),
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,3 +1,7 @@
 import { Redis } from "@upstash/redis";
+import {UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } from "astro:env/server";
 
-export const redis = Redis.fromEnv();
+export const redis = new Redis({
+    url: UPSTASH_REDIS_REST_URL,
+    token: UPSTASH_REDIS_REST_TOKEN,
+})
