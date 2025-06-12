a2d51132ffcdb00e333884d97aef699e49492d43
kkhys <kkhys@pm.me>
2025-06-12 13:26:49 +0900

    refactor(blog): use dynamic `NODE_ENV` in page view tracking
    Replaced hardcoded "production" string with dynamic `NODE_ENV` for tracking page views in Redis. Removed unused and commented code for better clarity.

--- src/features/blog/actions/page-view.ts
+++ src/features/blog/actions/page-view.ts
@@ -1,5 +1,5 @@
 import { defineAction } from "astro:actions";
-// import { NODE_ENV } from "astro:env/client";
+import { NODE_ENV } from "astro:env/client";
 import { z } from "astro:schema";
 import { redis } from "#/lib/redis";
 
@@ -8,12 +8,8 @@ export const pageView = {
     input: z.object({ slug: z.string() }),
     handler: async (input) => {
       const { slug } = input;
-      // const env = NODE_ENV;
-      // console.log("env", env);
       return (
-        (await redis.get<number>(
-          ["pageviews", "production", slug].join(":"),
-        )) ?? 0
+        (await redis.get<number>(["pageviews", NODE_ENV, slug].join(":"))) ?? 0
       );
     },
   }),
@@ -21,9 +17,7 @@ export const pageView = {
     input: z.object({ slug: z.string() }),
     handler: async (input) => {
       const { slug } = input;
-      // const env = NODE_ENV;
-      // console.log("env", env);
-      await redis.incr(["pageviews", "production", slug].join(":"));
+      await redis.incr(["pageviews", NODE_ENV, slug].join(":"));
     },
   }),
 };
