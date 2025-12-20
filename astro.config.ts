6453038ad6815be38f766d6b71d2812458758202
kkhys <kkhys@pm.me>
2025-12-20 20:52:37 +0900

    chore(config): add Partytown, sitemap, and compress integrations

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,17 @@
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig, envField } from "astro/config";
 
 export default defineConfig({
   site: "https://lgtm.kkhys.me",
+  integrations: [
+    sitemap(),
+    partytown(),
+    (await import("@playform/compress")).default({
+      Image: false,
+      SVG: false,
+    }),
+  ],
   env: {
     schema: {
       NODE_ENV: envField.enum({
