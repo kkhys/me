e259eda5c80f4a22e146ab5854713b2bdd152cb0
kkhys <kkhys@pm.me>
2026-01-02 22:41:00 +0900

    chore(seo): add Astro sitemap integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,7 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://kkhys.me",
+  integrations: [sitemap()],
 });
